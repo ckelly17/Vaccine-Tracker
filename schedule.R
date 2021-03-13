57df09628bdb514099f423dc2038d94b52e67419
Conor Kelly <conor.richard.kelly@gmail.com>
2021-03-13 15:33:09 -0500

    removing schedule

--- schedule.R
+++
@@ -1,8 +0,0 @@
-library(cronR)
-
-script <- "/Users/conorkelly/Documents/Vaccine-Tracker/vaccine_tracker.R"
-
-cmd <- cron_rscript(script)
-cron_clear()
-cron_add(command = cmd, frequency = 'hourly',
-         id = 'pull_cdc', description = 'pull CDC data', tags = c('cdc', 'R'))
